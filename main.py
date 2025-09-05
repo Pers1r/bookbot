f9ab149528b02823c5f8b96e797d6fee8dce5dce
Pers1r <Marek007.nowicki007@gmail.com>
2025-09-05 12:07:07 +0200

    Added get_sorted_caracters but without fancy printing

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import count_words, count_characters, get_sorted_characters
 
 
 def get_book_text(path):
@@ -9,6 +9,7 @@ def get_book_text(path):
 def main():
     book_text = get_book_text('books/frankenstein.txt')
     print(count_words(book_text), "words found in the document")
+    print(count_characters(book_text))
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,26 @@
 def count_words(text):
     return len(text.split())
+
+def count_characters(text):
+    dict = {}
+    for char in text:
+        if char.lower() in dict:
+            dict[char.lower()] += 1
+        else:
+            dict[char.lower()] = 1
+
+    return dict
+
+def get_sorted_characters(text):
+    dict = count_characters(text)
+    arr = []
+    for x in dict:
+        temp = {"char": x, "count": dict[x]}
+        arr.append(temp)
+
+    def sort_on(items):
+        return items["num"]
+
+    arr.sort(reverse=True, key=sort_on)
+
+    return arr
